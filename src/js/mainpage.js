849c3a821fc475a6b1c8d1db4e01f1a2abb74cfe
Jase Rader <jaserader@me.com>
2016-01-01 15:21:52 -0600

    added resume section, social nav and started portfolio section

--- src/js/mainpage.js
+++ src/js/mainpage.js
@@ -17,19 +17,67 @@ class Mainpage extends React.Component{
           <section id="aboutMe">
             <div id="headShot"></div>
             <div id="info">
-              <div>Jase is a Front-End Developer with a background in Information Technology and Buisness.</div>
+              <div>Jase is a Front-End Developer with a background in Information Technology and Business.</div>
 
-              <span id="resumeTitle">Resumé: <i className="fa fa-download"></i></span>
+              <span id="resumeTitle">Resumé: <span>(download PDF)</span><i className="fa fa-download"></i></span>
 
               <div id="briefResume">
                 <ul>
                   <li>Completed Iron Yard Front-End Engineering Bootcamp.</li>
                   <li>Graduated from the University of Tennessee in 2011 with a degree in Business Adminsitration - Logistics.</li>
-                  <li></li>
+                  <li>Front-End Skills - HTML, CSS, JavaScript, jQuery (or $), SASS, React, Backbone.js, GitHub</li>
+                  <li>Hobbies - Wood-Working, Tech Devices, Hiking, Traveling</li>
                 </ul>
               </div>
 
             </div>
+          </section>
+
+          <section id="socialNav">
+
+            <nav>
+              <Link to=""><i className="fa fa-github-square"></i></Link>
+              <Link to=""><i className="fa fa-linkedin-square"></i></Link>
+              <Link to=""><i className="fa fa-instagram"></i></Link>
+              <Link to=""><i className="fa fa-twitter"></i></Link>
+            </nav>
+
+          </section>
+
+          <section id="portfolio">
+
+            <section id="sectionOne">
+              <div id="outerDiv">
+                <div id="innerDiv"></div>
+              </div>
+
+              <div id="siteDescription"></div>
+            </section>
+
+            <section id="sectionOne">
+              <div id="outerDiv">
+                <div id="innerDiv"></div>
+              </div>
+
+              <div id="siteDescription"></div>
+            </section>
+
+            <section id="sectionOne">
+              <div id="outerDiv">
+                <div id="innerDiv"></div>
+              </div>
+
+              <div id="siteDescription"></div>
+            </section>
+
+            <section id="sectionOne">
+              <div id="outerDiv">
+                <div id="innerDiv"></div>
+              </div>
+
+              <div id="siteDescription"></div>
+            </section>
+
           </section>
         </div>
     )
