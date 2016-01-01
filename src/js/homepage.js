6ae7c422280c7bb502faaad7e4f6d29777cd3aeb
Jase Rader <jaserader@me.com>
2016-01-01 16:10:30 -0600

    added footer

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -7,7 +7,7 @@ class Homepage extends React.Component{
     return(
       <div id="homepage">
           <div id="logo"></div>
-          <div id="jobTitle">Front-End Developer</div>
+          <div id="jobTitle">Front-End Web Developer</div>
           <div id="contactNav"><Link><i className="fa fa-envelope-o"></i>jaserader@me.com</Link><Link><i className="fa fa-mobile"></i>615-838-5484</Link></div>
           <Link to="main" id="enterBtn">Enter</Link>
       </div>
--- src/js/mainpage.js
+++ src/js/mainpage.js
@@ -25,7 +25,7 @@ class Mainpage extends React.Component{
                 <ul>
                   <li>Completed Iron Yard Front-End Engineering Bootcamp.</li>
                   <li>Graduated from the University of Tennessee in 2011 with a degree in Business Adminsitration - Logistics.</li>
-                  <li>Front-End Skills - HTML, CSS, JavaScript, jQuery (or $), SASS, React, Backbone.js, GitHub</li>
+                  <li>Front-End Skills - HTML, CSS, JavaScript, jQuery (or $), SASS, React, Backbone.js, GitHub, Adobe Illustrator, Adobe Capture</li>
                   <li>Hobbies - Wood-Working, Tech Devices, Hiking, Traveling</li>
                 </ul>
               </div>
@@ -46,12 +46,14 @@ class Mainpage extends React.Component{
 
           <section id="portfolio">
 
+            <span id="portfolioTitle">Portfolio</span>
+
             <section id="sectionOne">
               <div id="outerDiv">
                 <div id="innerDiv"></div>
               </div>
 
-              <div id="siteDescription"></div>
+              <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
             </section>
 
             <section id="sectionOne">
@@ -59,7 +61,7 @@ class Mainpage extends React.Component{
                 <div id="innerDiv"></div>
               </div>
 
-              <div id="siteDescription"></div>
+              <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
             </section>
 
             <section id="sectionOne">
@@ -67,7 +69,7 @@ class Mainpage extends React.Component{
                 <div id="innerDiv"></div>
               </div>
 
-              <div id="siteDescription"></div>
+              <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
             </section>
 
             <section id="sectionOne">
@@ -75,10 +77,24 @@ class Mainpage extends React.Component{
                 <div id="innerDiv"></div>
               </div>
 
-              <div id="siteDescription"></div>
+              <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
             </section>
 
           </section>
+
+          <footer>
+            <nav>
+              <Link to=""><i className="fa fa-github-square"></i></Link>
+              <Link to=""><i className="fa fa-linkedin-square"></i></Link>
+              <Link to=""><i className="fa fa-instagram"></i></Link>
+              <Link to=""><i className="fa fa-twitter"></i></Link>
+            </nav>
+
+            <div id="downloads">
+              <Link><i className="fa fa-download"></i> Download Resumé</Link>
+              <Link><i className="fa fa-envelope-o"></i> Email Me</Link>
+            </div>
+          </footer>
         </div>
     )
   }
