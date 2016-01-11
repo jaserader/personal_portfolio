8971c5f939699763a9b9f0ba8ca53ab831e73eb0
Jase Rader <jaserader@me.com>
2016-01-11 09:30:19 -0600

    completed all media queries

--- src/js/mainpage.js
+++ src/js/mainpage.js
@@ -9,8 +9,8 @@ class Mainpage extends React.Component{
         <div id="mainpage">
           <header>
             <div id="smallLogo"></div>
-            <div id="headerNav"><Link><i className="fa fa-envelope-o"></i>jaserader@me.com</Link><Link><i className="fa fa-mobile"></i>615-838-5484</Link></div>
-            <div id="brief">Jase is a Front-End Developer with a background in Information Technology and Business.</div>
+            <div id="headerNav"><a href="mailto:jaserader@icloud.com"><i className="fa fa-envelope-o"></i>jaserader@me.com</a><a href="tel:615-838-5484"><i className="fa fa-mobile"></i>615-838-5484</a></div>
+            <div id="brief">Front-End Developer with a background in Information Technology and Business.</div>
           </header>
 
           <div id="heroImg"></div>
@@ -19,13 +19,13 @@ class Mainpage extends React.Component{
             <div id="headShot"></div>
             <div id="info">
 
-              <span id="resumeTitle">Resumé: <span>(download PDF)</span><i className="fa fa-download"></i></span>
+              <span id="resumeTitle">Resumé: <span>(download PDF)</span><a href="dist/img/resume.pdf" download="resume.pdf" className="fa fa-download"></a></span>
 
               <div id="briefResume">
                 <ul>
                   <li>Completed Iron Yard Front-End Engineering Bootcamp.</li>
                   <li>Graduated from the University of Tennessee in 2011 with a degree in Business Adminsitration - Logistics.</li>
-                  <li>Front-End Skills - HTML, CSS, JavaScript, jQuery (or $), SASS, React, Backbone.js, GitHub, Adobe Illustrator, Adobe Capture</li>
+                  <li>Front-End Skills - HTML, CSS, JavaScript, jQuery, SASS, React, Backbone.js, Underscore.js, Bourbon/Neat, Git, GitHub, Command Terminal, Bower, NPM, Gulp, Adobe Illustrator, Adobe Capture</li>
                   <li>Hobbies - Wood-Working, Tech Devices, Hiking, Traveling</li>
                 </ul>
               </div>
@@ -36,10 +36,10 @@ class Mainpage extends React.Component{
           <section id="socialNav">
 
             <nav>
-              <Link to=""><i className="fa fa-github-square"></i></Link>
-              <Link to=""><i className="fa fa-linkedin-square"></i></Link>
-              <Link to=""><i className="fa fa-instagram"></i></Link>
-              <Link to=""><i className="fa fa-twitter"></i></Link>
+              <a href="https://github.com/jaserader"><i className="fa fa-github-square"></i></a>
+              <a href="https://www.linkedin.com/in/jaserader"><i className="fa fa-linkedin-square"></i></a>
+              <a href="https://www.instagram.com/jase_rader/"><i className="fa fa-instagram"></i></a>
+              <a href="https://www.twitter.com"><i className="fa fa-twitter"></i></a>
             </nav>
 
           </section>
@@ -51,11 +51,6 @@ class Mainpage extends React.Component{
             <section id="sectionOne">
                 <div id="innerDiv"></div>
 
-              <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-            </section>
-
-            <section id="sectionOne">
-                <div id="innerDiv"></div>
 
               <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
             </section>
@@ -63,11 +58,20 @@ class Mainpage extends React.Component{
             <section id="sectionOne">
                 <div id="innerDiv"></div>
 
+
               <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
             </section>
 
             <section id="sectionOne">
                 <div id="innerDiv"></div>
+
+
+              <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            </section>
+
+            <section id="sectionOne">
+                <div id="innerDiv"></div>
+
 
               <div id="siteDescription"> Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
             </section>
@@ -76,15 +80,17 @@ class Mainpage extends React.Component{
 
           <footer>
             <nav>
-              <Link to=""><i className="fa fa-github-square"></i></Link>
-              <Link to=""><i className="fa fa-linkedin-square"></i></Link>
-              <Link to=""><i className="fa fa-instagram"></i></Link>
-              <Link to=""><i className="fa fa-twitter"></i></Link>
+              <a href="https://github.com/jaserader"><i className="fa fa-github-square"></i></a>
+              <a href="https://www.linkedin.com/in/jaserader"><i className="fa fa-linkedin-square"></i></a>
+              <a href="https://www.instagram.com/jase_rader/"><i className="fa fa-instagram"></i></a>
+              <a href="https://www.twitter.com"><i className="fa fa-twitter"></i></a>
             </nav>
 
+            <span id="copyright">© 2016 Jase Rader</span>
+
             <div id="downloads">
-              <Link><i className="fa fa-download"></i> Download Resumé</Link>
-              <Link><i className="fa fa-envelope-o"></i> Email Me</Link>
+              <a><i className="fa fa-download"></i> Download Resumé</a>
+              <a href="mailto:jaserader@icloud.com"><i className="fa fa-envelope-o"></i> Email Me</a>
             </div>
           </footer>
         </div>
